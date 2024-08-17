e15f288da62fb15a53f309ec052225fc3f46cbf8
dziegiel <jakubdzieg33@gmail.com>
2024-08-17 22:37:35 +0200

    program is partly working
    - analysing only first site of group members
    -slow af

--- main.py
+++ main.py
@@ -3,18 +3,13 @@ from bs4 import BeautifulSoup
 
 
 def check_if_banned(link):
-
     r = requests.get(link)
-
     soup = BeautifulSoup(r.content, 'html.parser')
-
     s = soup.find(name='div', class_='profile_ban_status')
-
     if s:
         return True
     else:
         return False
-
 
 def find_steam_links_in_string(string):
     results = re.findall(r'href=https://steamcommunity.com/(?:profiles/(\d+)|id/([\w-]+))', string)
@@ -26,12 +21,9 @@ def find_steam_links_in_string(string):
             links.append(f'https://steamcommunity.com/id/{i[1]}')
     return set(links)
 
-
 grouplink = str(input('paste steam group link '))
 
 grouplink = grouplink+'/members'
-
-print(grouplink)
 
 r = requests.get(grouplink)
 
@@ -43,5 +35,17 @@ s = str(s)
 
 s_without_qm = s.replace('"', '')
 
+links = find_steam_links_in_string(s_without_qm)
 
+banned = 0
+notbanned = 0
 
+for i in links:
+    if check_if_banned(i):
+        banned+=1
+    else:
+        notbanned+=1
+
+bannedpercentage = banned/(banned+notbanned)*100
+
+print(f'{bannedpercentage}%')
