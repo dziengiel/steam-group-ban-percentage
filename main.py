a0a69f325b91d9353fa664f23938544ef09b37a7
dziegiel <jakubdzieg33@gmail.com>
2024-08-17 17:07:39 +0200

    still not working
    -program is getting and scraping group members numbers but no id for this moment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests
+import requests, re
 from bs4 import BeautifulSoup
 
 
@@ -15,9 +15,38 @@ def check_if_banned(link):
     else:
         return True
 
+# def find_steam_links_in_string(string):
+#     string = string.split()
+#     links = set()
+#     for i in string:
+#         if i.startswith("href=https://steamcommunity.com/profiles/"):
+#             links.add(i)
+#     return links
+# #not banned
+# print(check_if_banned('https://steamcommunity.com/id/pstwo/'))
+#
+# #banned
+# print(check_if_banned('https://steamcommunity.com/profiles/76561199118069915'))
 
-#not banned
-print(check_if_banned('https://steamcommunity.com/id/pstwo/'))
 
-#banned
-print(check_if_banned('https://steamcommunity.com/id/psdwa'))
+grouplink = str(input('paste steam group link '))
+
+grouplink = grouplink+'/members'
+
+print(grouplink)
+
+r = requests.get(grouplink)
+
+soup = BeautifulSoup(r.content, 'html.parser')
+
+s = soup.find(name='div', id='memberList')
+
+s = str(s)
+
+s_without_qm = s.replace('"', '')
+
+links = re.findall(r'href=https://steamcommunity.com/profiles/(\d+)', s_without_qm)
+
+print(links)
+
+
